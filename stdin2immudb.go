a5f9558a1ac351190e092b40644b047c2114c4ba
Simone Lazzaris <slazzaris@gmail.com>
2021-07-08 16:30:11 +0200

    readback capability

--- stdin2immudb.go
+++ stdin2immudb.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
@@ -22,6 +23,7 @@ var config struct {
 	BatchSize int
 	Offset    int
 	Prefix    string
+	RBack     bool
 }
 
 func cfginit() {
@@ -33,6 +35,7 @@ func cfginit() {
 	flag.IntVar(&config.BatchSize, "batchsize", 1000, "Batch size")
 	flag.IntVar(&config.Offset, "offset", 0, "Initial counter value")
 	flag.StringVar(&config.Prefix, "prefix", "LINE", "Prefix for Key generation")
+	flag.BoolVar(&config.RBack, "readback", false, "Don't write, read back instead (and check value)")
 	flag.Parse()
 }
 
@@ -96,11 +99,61 @@ func inserter(ch chan string, out chan bool) {
 	out <- true
 }
 
+func checker(ch chan string, out chan bool) {
+	client, ctx := connect()
+	keys := make([][]byte, config.BatchSize)
+	vals := make([][]byte, config.BatchSize)
+	var idx = 0
+	var cnt = 0
+	t0 := time.Now()
+	for line := range ch {
+		keys[cnt] = []byte(fmt.Sprintf("%s%.9d", config.Prefix, idx+config.Offset))
+		vals[cnt] = []byte(line)
+		idx++
+		cnt++
+		if cnt == config.BatchSize {
+			readback, err := client.GetAll(ctx, keys)
+			if err != nil {
+				log.Fatalln("Failed to read the batch. Reason:", err)
+			} else {
+				log.Printf("Read %d lines", idx)
+			}
+			for j := 0; j < cnt; j++ {
+				if bytes.Compare(vals[j], readback.Entries[j].Value) != 0 {
+					log.Fatal("Mismatch %s <> %s", string(vals[j]), string(readback.Entries[j].Value))
+				}
+			}
+			cnt = 0
+		}
+
+	}
+	if cnt > 0 {
+		keys = keys[:cnt]
+		readback, err := client.GetAll(ctx, keys)
+		if err != nil {
+			log.Fatalln("Failed to read the batch. Reason:", err)
+		} else {
+			log.Printf("Read %d lines", idx)
+		}
+		for j := 0; j < cnt; j++ {
+			if bytes.Compare(vals[j], readback.Entries[j].Value) != 0 {
+				log.Fatal("Mismatch %s <> %s", string(vals[j]), string(readback.Entries[j].Value))
+			}
+		}
+	}
+	log.Printf("DONE: read %d lines in %s", idx, time.Now().Sub(t0))
+	out <- true
+}
+
 func main() {
 	cfginit()
 	ch := make(chan string)
 	out := make(chan bool)
-	go inserter(ch, out)
+	if config.RBack {
+		go checker(ch, out)
+	} else {
+		go inserter(ch, out)
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		ch <- scanner.Text()
